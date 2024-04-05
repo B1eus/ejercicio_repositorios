be5a8838e0588309302891c135234621d16bc158
B1eus <beurbanos@udistrital.edu.co>
2024-04-05 08:00:21 -0500

    actualizacion 2

--- src/ejemplo_clase_viernes/Test.java
+++ src/ejemplo_clase_viernes/Test.java
@@ -5,16 +5,31 @@ public class Test {
 	public static void main(String[] args) {
 		Persona tony_stark = new Persona ("Tony Stark", 35,"new york"); 
 		tony_stark.setEdad(35);
-		System.out.println(tony_stark.name+": "+tony_stark.edad+tony_stark.city);
-		System.out.println(tony_stark.name+": "+tony_stark.edad+tony_stark.city);
+		System.out.println("persona:  "+tony_stark.name+"  "+tony_stark.edad+"  "+tony_stark.city);
 		
 		
-		SuperHero iron_man = new SuperHero(tony_stark.name, tony_stark.edad);
+		SuperHero iron_man = new SuperHero(tony_stark.name, tony_stark.edad,null);
 				iron_man.heroname="Iron man";
-		iron_man.powers = new String[] {"volar","pelear"};
+		iron_man.powers = new String[] {"*volar","*pelear"};
 		iron_man.city = "orlando";
-	
-	
+		iron_man.franquicia = "Marvel";
+		
+		System.out.println("super heroe :" +"  "+iron_man.heroname+iron_man.city+"  "+iron_man.franquicia);
+		iron_man.mostrar_poderes();
+		//villano
+		Persona Arthur_Fleck = new Persona ("Arthur Fleck", 35,"Gothan"); 
+		Arthur_Fleck.setEdad(35);
+		System.out.println("persona:  "+Arthur_Fleck.name+"  "+Arthur_Fleck.edad+"  "+Arthur_Fleck.city);
+		
+		
+		Villano joker = new Villano(Arthur_Fleck.name, Arthur_Fleck.edad,null);
+				joker.Villan_name="joker";
+		joker.powers = new String[] {"*pelear"};
+		joker.city = "Gothan";
+		joker.franquicia = "Dc";
+		
+		System.out.println("super villano :"+"  "+joker.Villan_name+joker.city+"  "+joker.franquicia);
+		joker.mostrar_poderes();
 	}
 
 }
--- src/ejemplo_clase_viernes/Villano.java
+++ src/ejemplo_clase_viernes/Villano.java
@@ -1,19 +1,20 @@
 package ejemplo_clase_viernes;
 
-public class SuperHero extends Persona{
-	String heroname;
+public class Villano extends Persona {
+		
+	String Villan_name;
 	String powers[];
 	String franquicia;
-	//constructor 
-	public SuperHero(String name,int edad,String franquicia) {
-		super(name,edad, null);
+	public Villano(String name,int edad,String franquicia) {
+			super(name,edad, null);
 		
 	}
+		
 	public void mostrar_poderes() {
 		for (int i = 0;i<powers.length;i++) {
 			System.out.println(powers[i]);
-		}
-			
-	}
+		}	
+	
 
-}
+	}
+	}
